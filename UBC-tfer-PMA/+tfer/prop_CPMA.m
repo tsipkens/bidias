63583a75d95f5b9d9782ea795d06ef5b156c528d
Timothy A. Sipkens <tsipkens@gmail.com>
2019-06-13 21:35:59 -0400

    Added UBC-tfer-PMA folder

---
+++ UBC-tfer-PMA/+tfer/prop_CPMA.m
@@ -0,0 +1,60 @@
+function [prop] = prop_CPMA(opt)
+% PROP_CPMA Generates the prop struct used to summarize CPMA parameters.
+% 
+%-------------------------------------------------------------------------%
+% Input:
+%   opt         Options string specifying parameter set
+%                   (Optional, default 'Olfert')
+%
+% Output:
+%   prop        Properties struct for use in evaluating transfer function
+%-------------------------------------------------------------------------%
+
+
+if ~exist('opt','var') % if properties set is not specified
+    opt = 'Buckley';
+elseif isempty(opt)
+    opt = 'Buckley';
+end
+
+if strcmp(opt,'Olfert')
+    %-- CPMA parameters from Olfert lab --------------%
+    prop.r1 = 0.06; % inner electrode radius [m]
+    prop.r2 = 0.061; % outer electrode radius [m]
+    prop.L = 0.2; % length of chamber [m]
+    prop.p = 1; % pressure [atm]
+    prop.T = 293; % system temperature [K]
+    prop.Q = 3/1000/60;%0.3/1000/60;%1.5/1000/60; % volume flow rate (m^3/s) (prev: ~1 lpm)
+    prop.omega_hat = 32/33; % ratio of angular speeds
+
+elseif strcmp(opt,'Buckley')
+    %-- CPMA/APM parameters from Buckley et al. -------------%
+    prop.r2 = 0.025; % outer electrode radius [m]
+    prop.r1 = 0.024; % inner electrode radius [m]
+    prop.L = 0.1;    % length of APM [m]
+    RPM = 13350; % rotational speed [rpm]
+    prop.omega = RPM*2*pi/60; % rotational speed [rad/s]
+    prop.omega_hat = 1; % APM, so rotational speed is the same
+    prop.Q = 1.02e-3/60; % aerosol flowrate [m^3/s]
+    prop.T = 298; % system temperature [K]
+    prop.p = 1; % system pressure [atm]
+
+end
+
+
+%-- Parameters related to CPMA geometry ----------------------------------%
+prop.rc = (prop.r2+prop.r1)/2;
+prop.r_hat = prop.r1/prop.r2;
+prop.del = (prop.r2-prop.r1)/2; % half gap width
+
+prop.A = pi*(prop.r2^2-prop.r1^2); % cross sectional area of APM
+prop.v_bar = prop.Q/prop.A; % average flow velocity
+
+
+%-- For diffusion --------------------------------------------------------%
+kB = 1.3806488e-23; % Boltzmann's constant
+prop.D = @(B) kB.*prop.T.*B; % diffusion coefficient
+
+
+end
+
---
+++ UBC-tfer-PMA/+tfer/tfer_CPMA_B_pb.m
@@ -0,0 +1,57 @@
+
+function [Lambda,G0] = tfer_CPMA_B_pb(m_star,m,d,z,prop,varargin)
+% TFER_CPMA_B_PB Evaluates the transfer function for a CPMA in Case B (w/ parabolic flow).
+% Author:       Timothy Sipkens, 2019-03-21
+% 
+%-------------------------------------------------------------------------%
+% Inputs:
+%   m_star      Setpoint particle mass
+%   m           Particle mass
+%   d           Particle mobility diameter
+%   z           Integer charge state
+%   prop        Device properties (e.g. classifier length)
+%   varargin    Name-value pairs for setpoint    (Optional, default Rm = 3)
+%                   ('Rm',double) - Resolution
+%                   ('omega1',double) - Angular speed of inner electrode
+%                   ('V',double) - Setpoint voltage
+%
+% Outputs:
+%   Lambda      Transfer function
+%   G0          Function mapping final to initial radial position
+%-------------------------------------------------------------------------%
+
+tfer.get_setpoint; % get setpoint
+
+%-- Taylor series expansion constants ------------------------------------%
+C3 = tau.*(sp.alpha^2*prop.rc+2*sp.alpha*sp.beta/prop.rc+sp.beta^2/(prop.rc^3)-C0./(m.*prop.rc));
+C4 = tau.*(sp.alpha^2-2*sp.alpha*sp.beta/(prop.rc^2)-3*sp.beta^2/(prop.rc^4)+C0./(m.*(prop.rc^2)));
+
+A1 = -3*prop.v_bar./(4.*C4.^3.*prop.del^2);
+A2 = 2.*(C3.^2-C4.^2.*prop.del^2);
+A3 = @(r,ii) C4(ii).^2.*(r-prop.rc).^2-2.*C3(ii).*C4(ii).*(r-prop.rc);
+
+
+%-- Estimate equilibrium radius ------------------------------------------%
+if round((sqrt(C0./m_star)-sqrt(C0./m_star-4*sp.alpha*sp.beta))/(2*sp.alpha),15)==prop.rc
+    rs = real((sqrt(C0./m)-sqrt(C0./m-4*sp.alpha*sp.beta))./(2*sp.alpha)); % equiblirium radius for a given mass
+else
+    rs = real((sqrt(C0./m)+sqrt(C0./m-4*sp.alpha*sp.beta))./(2*sp.alpha)); % equiblirium radius for a given mass
+end
+
+
+%-- Set up F function for minimization -----------------------------------%
+F = @(r,ii) A1(ii).*(A2(ii).*log(abs(C4(ii).*(r-prop.rc)+C3(ii)))+A3(r,ii));
+min_fun = @(rL,r0,ii) F(rL,ii)-F(r0,ii)-prop.L;
+
+
+%-- Evaluate G0 and transfer function ------------------------------------%
+G0 = @(r) tfer.G_fun(min_fun,r,rs,prop.r1,prop.r2,sp.alpha,sp.beta);
+
+ra = min(prop.r2,max(prop.r1,G0(prop.r1)));
+rb = min(prop.r2,max(prop.r1,G0(prop.r2)));
+
+Lambda = 3/4.*(rb-ra)./prop.del-1/4.*((rb-prop.rc)./prop.del).^3+...
+    1/4.*((ra-prop.rc)./prop.del).^3;
+
+end
+
