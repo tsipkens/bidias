6f9dfe4163588b2f449835ae89488c09f4b7acc2
Timothy A. Sipkens <tsipkens@gmail.com>
2019-08-22 00:38:36 -0700

    Update to gitignore

--- submodule_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# SUBMODULE_UPDATE
-# Updates git submodules in current program (i.e. UBC-tfer-PMA)
-# Author:  Timothy Sipkens, 2019-07-15
-#===========================================================#
-
-git submodule update --recursive --remote
