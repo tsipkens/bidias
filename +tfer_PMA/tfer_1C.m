0a6d08e0108ba85eb82948d58b8aad632201bda4
Timothy A. Sipkens <tsipkens@gmail.com>
2019-09-04 22:55:18 +0100

    Update reference to tfer-PMA package

---
+++ +tfer_PMA/tfer_1C.m
@@ -0,0 +1,41 @@
+
+% TFER_1C	Evaluates the transfer function for a PMA in Case B.
+% Author:	Timothy Sipkens, 2019-03-21
+%=========================================================================%
+
+function [Lambda,G0] = tfer_1C(m_star,m,d,z,prop,varargin)
+%-------------------------------------------------------------------------%
+% Inputs:
+%   m_star      Setpoint particle mass
+%   m           Particle mass
+%   d           Particle mobility diameter
+%   z           Integer charge state
+%   prop        Device properties (e.g. classifier length)
+%   varargin    Name-value pairs for setpoint    (Optional, default Rm = 3)
+%                   ('Rm',double) - Resolution
+%                   ('omega1',double) - Angular speed of inner electrode
+%                   ('V',double) - Setpoint voltage
+%
+% Outputs:
+%   Lambda      Transfer function
+%   G0          Function mapping final to initial radial position
+%-------------------------------------------------------------------------%
+
+
+tfer_PMA.get_setpoint; % get setpoint (parses d and z)
+
+%-- Taylor series expansion constants ------------------------------------%
+C3 = tau.*(sp.alpha^2*prop.rc+2*sp.alpha*sp.beta/prop.rc+sp.beta^2/(prop.rc^3)-C0./(m.*prop.rc));
+C4 = tau.*(sp.alpha^2-2*sp.alpha*sp.beta/(prop.rc^2)-3*sp.beta^2/(prop.rc^4)+C0./(m.*(prop.rc^2)));
+
+
+%-- Evaluate G0 and transfer function ------------------------------------%
+G0 = @(r) prop.rc+(r-prop.rc+C3./C4).*exp(-C4.*prop.L./prop.v_bar)-C3./C4;
+
+ra = min(prop.r2,max(prop.r1,G0(prop.r1)));
+rb = min(prop.r2,max(prop.r1,G0(prop.r2)));
+
+Lambda = (1/(2*prop.del)).*(rb-ra);
+
+end
+
---
+++ +tfer_PMA/tfer_1S.m
@@ -0,0 +1,49 @@
+
+% TFER_1S   Evaluates the transfer function for a PMA in Case A.
+% Author:   Timothy Sipkens, 2018-12-27
+%=========================================================================%
+
+function [Lambda,G0] = tfer_1S(m_star,m,d,z,prop,varargin)
+%-------------------------------------------------------------------------%
+% Inputs:
+%   m_star      Setpoint particle mass
+%   m           Particle mass
+%   d           Particle mobility diameter
+%   z           Integer charge state
+%   prop        Device properties (e.g. classifier length)
+%   varargin    Name-value pairs for setpoint    (Optional, default Rm = 3)
+%                   ('Rm',double) - Resolution
+%                   ('omega1',double) - Angular speed of inner electrode
+%                   ('V',double) - Setpoint voltage
+%
+% Outputs:
+%   Lambda      Transfer function
+%   G0          Function mapping final to initial radial position
+%-------------------------------------------------------------------------%
+
+tfer_PMA.get_setpoint; % get setpoint (parses d and z)
+
+%-- Estimate equilibrium radius ------------------------------------------%
+if round((sqrt(C0./m_star)-sqrt(C0./m_star-4*sp.alpha*sp.beta))/(2*sp.alpha),15)==prop.rc
+    rs = real((sqrt(C0./m)-sqrt(C0./m-4*sp.alpha*sp.beta))./(2*sp.alpha));
+        % equiblirium radius for a given mass
+        
+else % else, use other root
+    rs = real((sqrt(C0./m)+sqrt(C0./m-4*sp.alpha*sp.beta))./(2*sp.alpha));
+end
+
+
+%-- Estimate device parameter --------------------------------------------%
+lam = 2.*tau.*(sp.alpha^2-sp.beta^2./(rs.^4)).*prop.L./prop.v_bar;
+
+
+%-- Evaluate G0 and transfer function ------------------------------------%
+G0 = @(r) rs+(r-rs).*exp(-lam);
+
+ra = min(prop.r2,max(prop.r1,G0(prop.r1)));
+rb = min(prop.r2,max(prop.r1,G0(prop.r2)));
+
+Lambda = (1/(2*prop.del)).*(rb-ra);
+
+end
+
