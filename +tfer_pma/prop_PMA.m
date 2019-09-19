ec65293b07ff913efd295afb762d0da7b0d6e2e4
Timothy A. Sipkens <tsipkens@gmail.com>
2019-09-19 15:28:06 -0700

    Forced git to recognize text case

---
+++ +tfer_pma/prop_PMA.m
@@ -0,0 +1,92 @@
+
+% PROP_CPMA Generates the prop struct used to summarize CPMA parameters.
+% Author:   Timothy Sipkens, 2019-06-26
+%=========================================================================%
+
+function [prop] = prop_PMA(opt)
+%-------------------------------------------------------------------------%
+% Input:
+%   opt         Options string specifying parameter set
+%                   (Optional, default 'Olfert')
+%
+% Output:
+%   prop        Properties struct for use in evaluating transfer function
+%-------------------------------------------------------------------------%
+
+
+if ~exist('opt','var') % if properties set is not specified
+    opt = 'Olfert';
+elseif isempty(opt)
+    opt = 'Olfert';
+end
+
+if strcmp(opt,'Olfert')
+    %-- CPMA parameters from Olfert lab --------------%
+    prop.r1 = 0.06; % inner electrode radius [m]
+    prop.r2 = 0.061; % outer electrode radius [m]
+    prop.L = 0.2; % length of chamber [m]
+    prop.p = 1; % pressure [atm]
+    prop.T = 293; % system temperature [K]
+    prop.Q = 3/1000/60;%0.3/1000/60;%1.5/1000/60; % volume flow rate (m^3/s) (prev: ~1 lpm)
+    prop.omega_hat = 32/33; % ratio of angular speeds
+
+elseif strcmp(opt,'Buckley')
+    %-- CPMA/APM parameters from Buckley et al. -------------%
+    prop.r2 = 0.025; % outer electrode radius [m]
+    prop.r1 = 0.024; % inner electrode radius [m]
+    prop.L = 0.1;    % length of APM [m]
+    prop.omega_hat = 1; % APM, so rotational speed is the same
+    prop.Q = 1.02e-3/60; % aerosol flowrate [m^3/s]
+    prop.T = 298; % system temperature [K]
+    prop.p = 1; % system pressure [atm]
+
+elseif strcmp(opt,'Ehara')
+    %-- APM parameters from Ehara et al. -------------%
+    prop.r2 = 0.103; % outer electrode radius [m]
+    prop.r1 = 0.1; % inner electrode radius [m]
+    prop.L = 0.2;    % length of APM [m]
+    prop.omega_hat = 1; % APM, so rotational speed is the same
+    prop.Q = 0.5/1000/60; % aerosol flowrate [m^3/s], assumed
+    prop.T = 298; % system temperature [K]
+    prop.p = 1; % system pressure [atm]
+
+elseif strcmp(opt,'Olfert-Collings')
+    %-- Parameters from Olfert and Collings -------------%
+    %   Nearly identical to the Ehara et al. case
+    prop.r2 = 0.103; % outer electrode radius [m]
+    prop.r1 = 0.1; % inner electrode radius [m]
+    prop.L = 0.2;
+    prop.omega_hat = 0.945;
+    prop.Q = 0.5/1000/60; % aerosol flowrate [m^3/s]
+    prop.T = 295; % system temperature [K]
+    prop.p = 1; % system pressure [atm]
+    
+elseif strcmp(opt,'Kuwata')
+    %-- Parameters from Kuwata --------------------------%
+    prop.r2 = 0.052; % outer electrode radius [m]
+    prop.r1 = 0.05; % inner electrode radius [m]
+    prop.L = 0.25;
+    prop.omega_hat = 1;
+    prop.Q = 1.67e-5; % aerosol flowrate [m^3/s]
+    prop.T = 295; % system temperature [K]
+    prop.p = 1; % system pressure [atm]
+    
+end
+
+
+%-- Parameters related to CPMA geometry ----------------------------------%
+prop.rc = (prop.r2+prop.r1)/2;
+prop.r_hat = prop.r1/prop.r2;
+prop.del = (prop.r2-prop.r1)/2; % half gap width
+
+prop.A = pi*(prop.r2^2-prop.r1^2); % cross sectional area of APM
+prop.v_bar = prop.Q/prop.A; % average flow velocity
+
+
+%-- For diffusion --------------------------------------------------------%
+kB = 1.3806488e-23; % Boltzmann's constant
+prop.D = @(B) kB.*prop.T.*B; % diffusion coefficient
+
+
+end
+
---
+++ +tfer_pma/tfer_W1.m
@@ -0,0 +1,49 @@
+
+% TFER_W1   Evaluates the transfer function for a PMA in Case E.
+% Author:   Timothy Sipkens, 2019-03-21
+%=========================================================================%
+
+function [Lambda,G0] = tfer_W1(m_star,m,d,z,prop,varargin)
+%-------------------------------------------------------------------------%
+% Inputs:
+%   m_star      Setpoint particle mass
+%   m           Particle mass
+%   d           Particle mobility diameter
+%   z           Integer charge state
+%   prop        Device properties (e.g. classifier length)
+%   varargin    Name-value pairs for setpoint    (Optional, default Rm = 3)
+%                   ('Rm',double) - Resolution
+%                   ('omega1',double) - Angular speed of inner electrode
+%                   ('V',double) - Setpoint voltage
+%
+% Outputs:
+%   Lambda      Transfer function
+%   G0          Function mapping final to initial radial position
+%-------------------------------------------------------------------------%
+
+
+tfer_pma.get_setpoint; % get setpoint (parses d and z)
+
+%-- Estimate equilibrium radius ------------------------------------------%
+if round((sqrt(C0./m_star)-sqrt(C0./m_star-4*sp.alpha*sp.beta))/(2*sp.alpha),15)==prop.rc
+    rs = real((sqrt(C0./m)-sqrt(C0./m-4*sp.alpha*sp.beta))./(2*sp.alpha)); % equiblirium radius for a given mass
+else
+    rs = real((sqrt(C0./m)+sqrt(C0./m-4*sp.alpha*sp.beta))./(2*sp.alpha)); % equiblirium radius for a given mass
+end
+
+
+%-- Estimate device parameter --------------------------------------------%
+lam = 2.*tau.*(sp.alpha^2-sp.beta^2./(rs.^4)).*prop.L./prop.v_bar;
+
+
+%-- Evaluate G0 and transfer function ------------------------------------%
+G0 = @(r) 1./(sp.omega1.*sqrt(m)).*...
+    sqrt((m.*sp.omega1^2.*r.^2-C0).*exp(-lam)+C0);
+
+ra = min(prop.r2,max(prop.r1,G0(prop.r1)));
+rb = min(prop.r2,max(prop.r1,G0(prop.r2)));
+
+Lambda = (1/(2*prop.del)).*(rb-ra);
+
+end
+
