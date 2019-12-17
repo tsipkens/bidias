85fa736a560dd3f3fc2b6240b8dce3106cb75eb7
Timothy A. Sipkens <tsipkens@gmail.com>
2019-12-17 11:58:40 -0800

    Minor update to +io package

--- +io/import_a.m
+++ +io/import_a.m
@@ -1,15 +1,18 @@
 
-% READ_A  Read tandem CPMA-SMPS data using SMPS data export and a CPMA log file.
+% IMPORT_A Read tandem CPMA-SMPS data using SMPS data export and a CPMA log file.
 % Author: Timothy Sipkens, Arash Naseri, 2019-12-17
 %=========================================================================%
 
-function [sp,prop_dma,prop_pma,out] = read_a(fn_smps,fn_cpma)
+function [sp,prop_dma,prop_pma,out] = import_a(fn_smps,fn_cpma)
 
 fid_smps = fopen(fn_smps);
 fid_cpma = fopen(fn_cpma);
 
 fclose(fid_smps);
 fclose(fid_cpma);
+
+sp.d_star = [];
+sp.m_star = [];
 
 end
 
