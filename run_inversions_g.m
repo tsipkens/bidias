8eba24c4ac7c1fd60d235846a78e390f684b5e84
Timothy A. Sipkens <tsipkens@gmail.com>
2020-02-04 13:52:10 -0800

    Split optimize folder
    optimize_a - Optimization methods for when the true solution is known.
    optimize_b - Optimization methods for when the true solution is NOT known. (e.g. Bayes factor method)

--- run_inversions_g.m
+++ run_inversions_g.m
@@ -6,7 +6,7 @@
 %-- Tikhonov (1st order) -----%
 disp('Performing Tikhonov (1st) regularization...');
 tic;
-[x_tk1,lambda_tk1,out_tk1] = optimize.tikhonov_op(...
+[x_tk1,lambda_tk1,out_tk1] = optimize_a.tikhonov_op(...
     Lb*A,Lb*b,n_x(1),[1e-2,1e1],x0,1);
 t.tk1 = toc;
 disp('Inversion complete.');
