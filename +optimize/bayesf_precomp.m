0c96858ab94d08dbe08a6ba98b1fc08473fca956
Timothy A. Sipkens <tsipkens@gmail.com>
2020-02-27 17:42:46 -0800

    Added bayesf_b
    The added bayesf_b incorporates potential changes in the data uncertainty weighting (to be used in conjunction with marginal distribution measurements).
    + Updates to commenting in other bayesf* methods.

--- +optimize/bayesf_precomp.m
+++ +optimize/bayesf_precomp.m
@@ -21,19 +21,17 @@ if isempty(S1)
     [~,~,~,S1,S2] = gsvd(full(A),full(Lpr0));
 end
 
-F = -1/2*(norm(A*x-b)^2 + norm(lambda.*Lpr0*x)^2);
-
-% Gpo_inv = (A')*A+lambda^2.*(Lpr')*Lpr;
-% C = (length(x)-order)*log(lambda) - tools.logdet(Gpo_inv)/2;
+F = -1/2*(norm(A*x-b)^2 + norm(lambda.*Lpr0*x)^2); % fit
 
 h = max(S1,[],2); % picks off diagonal elements of each row
 the = diag(S2);
 
 ct = 2*sum(log(the((lx-r+1):lb))); % contributes to the constant term
-% cp = 2*tools.logdet(P); % term cancels out between |Lpr| and |Gpo|
 det_po = sum(log(h((lx-r+1):lb).^2 + lambda^2.*the((lx-r+1):lb).^2));
 
 C = (r+lb-lx)*log(lambda) - det_po/2 + ct/2;
+    % measurement credence
+    % ignores det_b contributions
 
 B = F+C;
 
