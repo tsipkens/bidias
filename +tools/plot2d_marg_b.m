e5891fa12ec721423b2e8153d210b7320ca1fe54
Timothy A. Sipkens <tsipkens@gmail.com>
2020-03-09 12:42:56 -0700

    Update plot2d_marg_b.m
    Based on sugggestions by @ArashNaseri.

--- +tools/plot2d_marg_b.m
+++ +tools/plot2d_marg_b.m
@@ -1,9 +1,9 @@
 
 % PLOT2D_MARG_B Plots x as a 2D function on the grid, with marginalized distributions.
-% Author: Timothy Sipkens, 2020-02-03
+% Author: Timothy Sipkens, Arash Naseri 2020-02-03
 %==================================================================%
 
-function [h,x_m] = plot2d_marg_b(grid,x,obj_t,x_t)
+function [h,x_m] = plot2d_marg_b(grid,x,d,obj_t,x_t)
 
 subplot(4,6,[8,23]);
 grid.plot2d(x);
@@ -15,11 +15,11 @@ subplot(4,6,[2,5]);
 marg_dim = 2;
 stairs(grid.nodes{marg_dim},...
     [x_m{marg_dim},0],'k');
+
 xlim([min(grid.edges{marg_dim}),max(grid.edges{marg_dim})]);
 set(gca,'XScale','log');
-
 ylabel({'d{\itN}/dlog {\itm}_{p}'},'Rotation',90)
-if nargin>2 % also plot marginal of the true distribution
+if nargin>3 % also plot marginal of the true distribution
     x_m_t = obj_t.marginalize(x_t);
 
     hold on;
@@ -36,8 +36,18 @@ stairs([0;x_m{marg_dim}],...
 ylim([min(grid.edges{marg_dim}),max(grid.edges{marg_dim})]);
 set(gca,'YScale','log');
 
+
+%-- Plot marginal SP2 data ----------------------------%
+if and(nargin>3, exist('d','var'))
+    hold on;
+    stairs(d,grid.edges{1, 1},'b');
+    hold off;
+end
+
+
+%-- Plot true distribution ----------------------------%
 xlabel({'d{\itN}/dlog {\itm}_{rBC}'},'Rotation',0);
-if nargin>2 % also plot marginal of the true distribution
+if nargin>3 % also plot marginal of the true distribution
     hold on;
     plot([0;x_m_t{marg_dim}],...
         obj_t.nodes{marg_dim},'color',[0.6,0.6,0.6]);
