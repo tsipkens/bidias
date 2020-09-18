7c01da367549e9ab485405d8825e35a22b620ccc
Timothy A. Sipkens <tsipkens@gmail.com>
2020-09-18 10:43:56 -0700

    Added loop over s in tools.overlay_ellipse

--- +tools/overlay_ellipse.m
+++ +tools/overlay_ellipse.m
@@ -23,29 +23,30 @@ if isempty(varargin); varargin = {'w'}; end
 if ~exist('s','var'); s = []; end
 if isempty(s); s = 1; end
 
-
-for ii=1:size(Sigma0,3) % if multiple modes are provided, loop through them
-    mu = mu0(ii,:);
-    Sigma = Sigma0(:,:,ii);
-    
-    mu = fliplr(mu(:)'); % flip mean and covar., (:)' ensures row format
-    Sigma = rot90(Sigma,2);
-
-    s = s.*2; % double number of std. dev. for ellipse
-              % (i.e. one std. dev. in each direction)
-
-    [V, D] = eig(Sigma.*s);
-
-    t = linspace(0, 2*pi);
-    a = (V*sqrt(D))*[cos(t(:))'; sin(t(:))'];
-
-    hold on;
-    if strcmp(get(gca,'XScale'),'log')
-        h = loglog(10.^(a(1,:)+mu(1)), 10.^(a(2,:)+mu(2)), varargin{:});
-    else
-        h = plot(a(1,:)+mu(1), a(2,:)+mu(2), varargin{:});
+for jj=1:length(s) % loop over multiple standard deviations, if relevant
+    for ii=1:size(Sigma0,3) % if multiple modes are provided, loop through them
+        mu = mu0(ii,:);
+        Sigma = Sigma0(:,:,ii);
+        
+        mu = fliplr(mu(:)'); % flip mean and covar., (:)' ensures row format
+        Sigma = rot90(Sigma,2);
+        
+        s(jj) = s(jj).*2; % double number of std. dev. for ellipse
+                  % (i.e. one std. dev. in each direction)
+        
+        [V, D] = eig(Sigma.*s(jj));
+        
+        t = linspace(0, 2*pi); % angles around ellipse to plot
+        a = (V*sqrt(D)) * [cos(t(:))'; sin(t(:))']; % points on ellipse
+        
+        hold on;
+        if strcmp(get(gca,'XScale'),'log') % account for whether logscale
+            h = loglog(10.^(a(1,:)+mu(1)), 10.^(a(2,:)+mu(2)), varargin{:});
+        else
+            h = plot(a(1,:)+mu(1), a(2,:)+mu(2), varargin{:});
+        end
+        hold off;
     end
-    hold off;
 end
 
 if nargout==0; clear h; end % remove output if none requested
