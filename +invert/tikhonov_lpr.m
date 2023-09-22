117f34c05355e1008cabb0b69b751aebfb7bd1a1
Timothy Sipkens <tsipkens@uwaterloo.ca>
2023-09-22 15:57:44 -0400

    Add tikhonov 1.3
    A first order Tikhonov matrix that smooths in slope 3 direction (as opposed to the default 45 deg.).

--- +invert/tikhonov_lpr.m
+++ +invert/tikhonov_lpr.m
@@ -12,7 +12,7 @@
 %   Lpr0        Tikhonov matrix
 %=========================================================================%
 
-function Lpr0 = tikhonov_lpr(order,n_grid,x_length)
+function Lpr0 = tikhonov_lpr(order, n_grid, x_length)
 
 if ~exist('order','var'); order = []; end
 if isempty(order); order = 1; end
@@ -35,18 +35,37 @@ switch order
     case 1 % 1st order Tikhonov
         if isa(n_grid,'Grid'); Lpr0 = n_grid.l1; % use Grid method (for partial grid support)
         else
-            I1 = 0.5.*speye(n,n);
-            E1 = full(sparse(1:n-1,2:n,1,n,n));
+            I1 = 0.5 .* speye(n, n);
+            E1 = full(sparse(1:n-1, 2:n, 1, n, n));
             D1 = E1-I1;
 
-            m = x_length/n;
-            I2 = 0.5.*speye(m,m);
-            E2 = sparse(1:m-1,2:m,1,m,m);
-            D2 = E2-I2;
+            m = x_length / n;
+            I2 = 0.5 .* speye(m, m);
+            E2 = sparse(1:m-1, 2:m, 1, m, m);
+            D2 = E2 - I2;
 
-            Lpr0 = kron(I2,D1)+kron(D2,I1);
+            Lpr0 = kron(I2, D1) + kron(D2, I1);
 
-            Lpr0 = Lpr0-spdiags(sum(Lpr0,2),0,x_length,x_length);
+            Lpr0 = Lpr0 - spdiags(sum(Lpr0,2), 0, x_length, x_length);
+            Lpr0(end,:) = [];
+        end
+        
+    case 1.3  % 1st order Tikhonov with rotated matrix
+        slope = 3;
+        if isa(n_grid,'Grid'); Lpr0 = n_grid.l1(slope); % use Grid method (for partial grid support)
+        else
+            I1 = 1/2 .* speye(n, n);
+            E1 = full(sparse(1:n-1, 2:n, 1, n, n));
+            D1 = E1-I1;
+    
+            m = x_length / n;
+            I2 = slope/2 .* speye(m, m);
+            E2 = sparse(1:m-1, 2:m, 1, m, m);
+            D2 = E2 - I2;
+    
+            Lpr0 = kron(I2, D1) + kron(D2, I1);
+    
+            Lpr0 = Lpr0 - spdiags(sum(Lpr0,2), 0, x_length, x_length);
             Lpr0(end,:) = [];
         end
         
@@ -57,15 +76,16 @@ switch order
             E1 = sparse(1:n-1,2:n,1,n,n);
             D1 = E1+E1'-I1;
 
-            m = x_length/n;
-            I2 = 0.25.*speye(m,m);
-            E2 = sparse(1:m-1,2:m,1,m,m);
+            m = x_length / n;
+            I2 = 0.25.*speye(m, m);
+            E2 = sparse(1:m-1, 2:m, 1, m, m);
             D2 = E2+E2'-I2;
 
-            Lpr0 = kron(I2,D1)+kron(D2,I1);
-            Lpr0 = Lpr0-spdiags(sum(Lpr0,2),0,x_length,x_length);
+            Lpr0 = kron(I2, D1)+kron(D2, I1);
+            Lpr0 = Lpr0 - spdiags(sum(Lpr0,2), 0, x_length, x_length);
             
         end
+
     otherwise
         disp('The specified order of Tikhonov is not available.');
         disp(' ');
